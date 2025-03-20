7c456bef54cd4f874d5aa1c08c800cf48acf3729
samim23 <s@samim.io>
2025-03-20 23:50:13 +0100

    import roleback

--- geminigen.py
+++ geminigen.py
@@ -9,8 +9,8 @@ import asyncio
 from io import BytesIO
 from PIL import Image
 from tqdm import tqdm
-import google.generativeai as genai
-from google.generativeai import types
+from google import genai
+from google.genai import types
 import json
 import re
 from datetime import datetime
